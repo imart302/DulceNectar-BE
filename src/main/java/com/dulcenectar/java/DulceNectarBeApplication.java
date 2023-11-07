15ad3c78c5a3d661ea0c3eb75070f3c8b6ec7f4b
Ivan Martinez <imartinezchin@gmail.com>
2023-11-07 17:41:55 -0600

    feat(cors): allow origins

--- src/main/java/com/dulcenectar/java/DulceNectarBeApplication.java
+++ src/main/java/com/dulcenectar/java/DulceNectarBeApplication.java
@@ -3,11 +3,13 @@ package com.dulcenectar.java;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class DulceNectarBeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DulceNectarBeApplication.class, args);
 	}
-
+	
+	
 }
---
+++ src/main/java/com/dulcenectar/java/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.dulcenectar.java.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        // You can customize these settings to meet your specific requirements
+        config.addAllowedOriginPattern("*");
+        config.applyPermitDefaultValues();
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
